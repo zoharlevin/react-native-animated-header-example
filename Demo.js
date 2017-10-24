5a674862927a03ef46369fbbbc27675e421c2dd6
Zohar Levin <zohar@soluto.com>
2017-10-24 15:30:39 +0300

    snap to edges of animation so user can't stop mid-animation

--- Demo.js
+++ Demo.js
@@ -8,36 +8,59 @@ export const scrollRangeForAnimation = 100;
 
 const HeaderPlaceholder = <View style={{flex: 0, height: 200, width: '100%'}} />;
 
-const Demo = ({scrollY, animationRange}) =>   
-      <View style={styles.container}>                              
-        <Animated.ScrollView            
-            style={styles.scrollView}
-            onScroll={Animated.event(
-                [
+const Demo = ({scrollY, animationRange}) => {  
+    let _scrollView = null;
+
+    const onScrollEndSnapToEdge = event => {
+        const y = event.nativeEvent.contentOffset.y;
+        if (0 < y && y < scrollRangeForAnimation / 2) {
+            if (_scrollView) {
+                _scrollView.scrollTo({y: 0});
+            }
+        } else if (scrollRangeForAnimation / 2 <= y && y < scrollRangeForAnimation) {
+            if (_scrollView) {
+                _scrollView.scrollTo({y: scrollRangeForAnimation});
+            }
+        }
+    };
+
+      return (
+        <View style={styles.container}>                              
+            <Animated.ScrollView            
+                style={styles.scrollView}
+                ref={scrollView => {
+                    _scrollView = scrollView ? scrollView._component : null;
+                }}
+                onScrollEndDrag={onScrollEndSnapToEdge}
+                onMomentumScrollEnd={onScrollEndSnapToEdge}
+                onScroll={Animated.event(
+                    [
+                        {
+                            nativeEvent: {contentOffset: {y: scrollY}},
+                        },
+                    ],
                     {
-                        nativeEvent: {contentOffset: {y: scrollY}},
-                    },
-                ],
-                {
-                    useNativeDriver: true,
-                }
-            )}
-            >
-            {HeaderPlaceholder}
-            <ItemInScroll numberOfItem={1} />
-            <ItemInScroll numberOfItem={2} />
-            <ItemInScroll numberOfItem={3} />
-            <ItemInScroll numberOfItem={4} />
-            <ItemInScroll numberOfItem={5} />
-            <ItemInScroll numberOfItem={6} />
-            <ItemInScroll numberOfItem={7} />
-            <ItemInScroll numberOfItem={8} />
-            <ItemInScroll numberOfItem={9} />
-            <ItemInScroll numberOfItem={10} />
-            <ItemInScroll numberOfItem={11} />
-        </Animated.ScrollView>         
-        <AnimatedHeader animationRange={animationRange}/>        
-      </View>    
+                        useNativeDriver: true,
+                    }
+                )}
+                >
+                {HeaderPlaceholder}
+                <ItemInScroll numberOfItem={1} />
+                <ItemInScroll numberOfItem={2} />
+                <ItemInScroll numberOfItem={3} />
+                <ItemInScroll numberOfItem={4} />
+                <ItemInScroll numberOfItem={5} />
+                <ItemInScroll numberOfItem={6} />
+                <ItemInScroll numberOfItem={7} />
+                <ItemInScroll numberOfItem={8} />
+                <ItemInScroll numberOfItem={9} />
+                <ItemInScroll numberOfItem={10} />
+                <ItemInScroll numberOfItem={11} />
+            </Animated.ScrollView>         
+            <AnimatedHeader animationRange={animationRange}/>        
+        </View>    
+      );
+}
 
 const styles = StyleSheet.create({
   container: {
